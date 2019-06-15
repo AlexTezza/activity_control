6c53e2a850f21ab749f45874f88aabda9e899661
Alex Tezza <tezzaalex@gmail.com>
2019-06-15 14:48:15 -0300

    (myTime) - Finalizada a implementação dos Dashboards gerenciais da aplicação, onde será possível filtrar a pesquisa por mês e ano.

--- backend/api/colaboratorsChart.js
+++ backend/api/colaboratorsChart.js
@@ -7,6 +7,19 @@ module.exports = app => {
         existsOrError(req.params.dateFrom)
         existsOrError(req.params.dateUntil)
 
+        let dateFrom = req.params.dateFrom
+        let dateUntil = req.params.dateUntil
+
+        let organizedResultData = await colaboratorHour(res, dateFrom, dateUntil)
+        let rawHours = await totalHoursPerAcvitiesType(res, dateFrom, dateUntil)
+        let optionsSeries = extractOptionsAndSeries(rawHours)
+        let totals = returnTotalHoursObject(rawHours)
+        let rawResultData = formatHourPerActivitiesType(rawHours)
+
+        return res.json({ organizedResultData, rawResultData, totals, optionsSeries })
+    }
+
+    async function colaboratorHour(res, dateFrom, dateUntil) {
         let query = `
             select
                 u.id as idUsuario,
@@ -17,32 +30,93 @@ module.exports = app => {
                 foo.duracao
             from
                 (
-                select
-                    a."idUsuario" as idUsuario,
-                    a."idTipoAtividade" as idTipoAtividade,
-                    sum(a.duracao) as duracao
-                from
-                    atividade a
-                    inner join "tipoAtividade" ta on ta.id = a."idTipoAtividade"
-                    inner join usuario u on u.id = a."idUsuario"
-                where
-                    data between '${req.params.dateFrom}'
-                    and '${req.params.dateUntil}'
-                group by
-                    a."idUsuario",
-                    a."idTipoAtividade"
-                ) foo
+                    select
+                        a."idUsuario" as idUsuario,
+                        a."idTipoAtividade" as idTipoAtividade,
+                        sum(a.duracao) as duracao
+                    from
+                        atividade a
+                        inner join "tipoAtividade" ta on ta.id = a."idTipoAtividade"
+                        inner join usuario u on u.id = a."idUsuario"
+                    where
+                        data between '${dateFrom}'
+                        and '${dateUntil}'
+                    group by
+                        a."idUsuario",
+                        a."idTipoAtividade"
+                    ) foo
                 inner join usuario u on u.id = foo.idUsuario
                 inner join "tipoAtividade" ta on ta.id = foo.idTipoAtividade
-                order by u.nome, ta.descricao
+            order by u.nome, ta.descricao
         `
-        await app.db.raw(query)
-        .then(result => organizeData(result.rows))
-        .then(result => res.json(result))
-        .catch(err => res.status(500).send(err))
+        return await app.db.raw(query)
+            .then(result => organizeData(result.rows))
+            .catch(err => res.status(500).send(err))
     }
 
-    function organizeData(data) {
+    async function totalHoursPerAcvitiesType(res, dateFrom, dateUntil) {
+        let query = `
+            select
+                ta.descricao as description,
+                sum(a.duracao) as total
+            from
+                atividade a
+                inner join "tipoAtividade" ta on ta.id = a."idTipoAtividade"
+                inner join usuario u on u.id = a."idUsuario"
+            where
+                data between '${dateFrom}'
+                and '${dateUntil}'
+            group by
+                ta.descricao
+            order by ta.descricao
+        `
+        return await app.db.raw(query)
+            .then(result => result.rows)
+            .catch(err => 
+                res.status(500).send(err)
+            )
+    }
+
+    function extractOptionsAndSeries(items) {
+        let optionsArray = []
+        let seriesArray = []
+        items.map(item => {
+            optionsArray.push(item.description)
+            seriesArray.push(parseInt(item.total))
+        })
+
+        return {
+            options: optionsArray,
+            series: seriesArray
+        }
+    }
+
+    function returnTotalHoursObject(items) {
+        let totalMinutes = sumTotalHours(items)
+        return {
+            description: "Total de horas realizadas",
+            totalHours: formatterMinutesToHours(totalMinutes),
+            totalMinutes: totalMinutes
+        }
+    }
+
+    function formatHourPerActivitiesType(items) {
+        items.map(item => {
+            item.total = formatterMinutesToHours(item.total)
+        })
+        return items
+    }
+
+    function sumTotalHours(items) {
+        let total = 0
+        items.map(item => {
+            total += parseInt(item.total)
+        })
+
+        return total
+    }
+
+    function organizeData(data, minutesToHours = false) {
         let resultArray = []
         const users = getAllUsersFromData(data);
 
--- frontend/src/components/home/charts/admin/ColaboratorsChart.vue
+++ frontend/src/components/home/charts/admin/ColaboratorsChart.vue
@@ -38,11 +38,16 @@
 							</div>
 						</b-form-row>
 						<div slot="footer" class="float-right">
-							<b-button variant="danger" class="mr-1" @click="resetSearch">
+							<b-button
+								variant="light"
+								class="mr-1"
+								@click="resetSearch">
 								<i class="fa fa-remove"></i>
 								Limpar
 							</b-button>
-							<b-button variant="primary" @click="loadData">
+							<b-button
+								variant="primary"
+								@click="loadData">
 								<i class="fa fa-search"></i>
 								Pesquisar
 							</b-button>
@@ -51,53 +56,54 @@
 				</b-card-group>
 			</b-collapse>
 		</div>	
+		<b-card-group class="pt-4">		
+			<b-card class="table-hour-geral" deck>
+				<b-table
+					:fields=fields
+					:items=itemsTableGeral
+					small
+					bordered
+					responsive
+					hover
+					striped
+				>
+					<template slot="thead-top">
+						<tr>
+							<th class="table-title" :colspan="fields.length">RELATÓRIO GERENCIAL</th>
+						</tr>
+						<tr>
+							<th class="month-title" :colspan="fields.length">{{ searchedMonth }}</th>
+						</tr>
+					</template>
+				</b-table>
 
-		<b-card class="table-hour-geral" deck>
-			<b-table
-				:fields=fields
-				:items=itemsTableGeral
-				small
-				bordered
-				responsive
-				hover
-				striped
-			>
-				<template slot="thead-top">
-					<tr>
-						<th class="table-title" :colspan="fields.length">RELATÓRIO GERENCIAL</th>
-					</tr>
-					<tr>
-						<th class="month-title" :colspan="fields.length">{{ searchedMonth }}</th>
-					</tr>
-				</template>
-			</b-table>
+				<hr>
 
-			<hr>
-
-			<div>
-				<b-card-group deck>
-					<b-card>
-						<h3>Totais</h3>
-						<table class="table table-hover">
-							<tbody v-for="(item) in totalHoursPerActivity" v-bind:key="item.key">
-								<tr>
-									<th>{{ item.label }}</th>
-									<th>{{ item.total }}</th>
+				<div>
+					<b-card-group deck>
+						<b-card>
+							<h3>Totais</h3>
+							<table class="table table-hover">
+								<tbody v-for="(item) in totalHoursPerActivity" v-bind:key="item.key">
+									<tr>
+										<td>{{ item.description }}</td>
+										<td>{{ item.total }}</td>
+									</tr>
+								</tbody>
+								<tr class="total-hours">
+									<th>{{ totalHours.description }}</th>
+									<th>{{ totalHours.totalHours }}</th>
 								</tr>
-							</tbody>
-							<tr>
-								<th>Total de horas realizadas</th>
-								<th>25h43min</th>
-							</tr>
-						</table>
-					</b-card>
-					<b-card>
-						<h3>Horas Faturadas X Horas Não Faturadas</h3>
-						<apexchart width="80%" type="donut" :options="chartDonutOptions" :series="donutSeries"></apexchart>
-					</b-card>
-				</b-card-group>
-			</div>
-		</b-card>
+							</table>
+						</b-card>
+						<b-card>
+							<h3>Percentual de horas por tipo de atividade</h3>
+							<apexchart type="donut" :options="chartDonutOptions" :series="donutSeries"></apexchart>
+						</b-card>
+					</b-card-group>
+				</div>
+			</b-card>
+		</b-card-group>
 	</div>
 </template>
 
@@ -105,29 +111,20 @@
 import axios from 'axios'
 import { baseApiUrl } from '@/global'
 import moment from 'moment'
+import { minutesToHours, percentualPerActivity } from '../../../utils'
 
 const months = moment.localeData('pt-BR').months()
 const years = [ "2019" ]
 
 const initialSearch = {
-    actualMonth : moment().month(),
+    actualMonth : 3,
     actualYear : moment().year()
 }
 
-function formatterTooltip(val) {
-	if (val) {
-		let hours = val.toString().split('.')[0]
-		let minutes = val.toString().split('.')[1]
-		if (minutes && minutes !== '00') {
-			if (minutes.length === 1) {
-				minutes = minutes + "0"
-			}
-			return hours + "h" + minutes + "min"
-		}
-		return hours + "h"
-	}
-	return ""
-}
+const defaultText = 'Nenhum dado para ser exibido'
+
+let totalHoursRealized = null
+let totalMinutesRealized = null
 
 export default {
     name: 'ColaboratorsChart',
@@ -139,31 +136,21 @@ export default {
 			search : { ...initialSearch },
 			searchedMonth: '',
 			itemsTableGeral: [],
-			totalHoursPerActivity: [
-				{ key : 1, label : "Break", total : "25h43min" },
-				{ key : 2, label : "Atividade externa pessoal", total : "25h43min" },
-				{ key : 3, label : "Férias", total : "25h43min" },
-				{ key : 4, label : "Atestado médico", total : "25h43min" },
-				{ key : 5, label : "Horas Faturadas", total : "25h43min" },
-				{ key : 6, label : "Processos Internos", total : "25h43min" },
-				{ key : 7, label : "Folga", total : "25h43min" },
-				{ key : 8, label : "Licença", total : "25h43min" },
-				{ key : 9, label : "Feriado", total : "25h43min" },
-				{ key : 10, label : "Desenvolvimento", total : "25h43min" },
-				{ key : 11, label : "Análise", total : "25h43min" },
-				{ key : 12, label : "Code Review", total : "25h43min" },
-				{ key : 13, label : "Request for proposal", total : "25h43min" }
-			],
+			totalHours: {},
+			totalHoursPerActivity: [],
 			// Donut Chart
 			chartDonutOptions: {
-				labels: ["Horas Faturadas", "Hora Não Faturadas"],
+				labels: [],
 				legend: {
 					position: 'left',
+					formatter: function(val, serie) {
+						return percentualPerActivity(val, serie, totalMinutesRealized)
+					}
 				},
 				tooltip: {
 					y: {
 						formatter: function(val) {
-							return formatterTooltip(val)
+							return minutesToHours(val)
 						}
 					}
 				},
@@ -174,24 +161,42 @@ export default {
 				},
 				plotOptions: {
 					pie: {
+						expandOnClick: true,
+						dataLabels: {
+							offset: 0,
+							minAngleToShowLabel: 3
+						},
 						donut: {
 							labels: {
-								show: true,
 								name: {
-									show: true
+									show: true,
+									color: '#304758',
 								},
 								value: {
 									show: true,
 									formatter: function(val) {
-										return formatterTooltip(val)
+										return minutesToHours(val)
 									}
 								},
+								total: {
+									show: true,
+									formatter: function() {
+										return totalHoursRealized
+									}
+								}
 							}
 						}
 					}
 				}
 			},
-			donutSeries: [44, 55]
+			chartSerieName: [],
+			donutSeries: [0],
+			chartDonutData: [],
+		}
+	},
+	computed: {
+		icon() {
+			return this.$store.state.isCollapseVisible ? "fa-angle-up" : "fa-angle-down"
 		}
 	},
 	methods: {
@@ -211,9 +216,29 @@ export default {
 
 			const url = `${baseApiUrl}/colaborators-chart/${dateFrom}/${dateUntil}`
 			axios.get(url).then(res => {
-				this.itemsTableGeral = res.data
+				
+				this.chartSerieName = [defaultText]
+				this.chartDonutData = [0]
+				totalHoursRealized = 0
+				totalMinutesRealized = 0
 
+				this.itemsTableGeral = res.data.organizedResultData
 				this.searchedMonth = months[this.search.actualMonth]
+				this.totalHours = res.data.totals
+				totalHoursRealized = res.data.totals ? res.data.totals.totalHours : 0
+				totalMinutesRealized = res.data.totals ? res.data.totals.totalMinutes : 0
+				this.totalHoursPerActivity = res.data.rawResultData
+
+				if (res.data && res.data.optionsSeries
+						&& res.data.optionsSeries
+						&& res.data.optionsSeries.options.length > 0
+						&& res.data.optionsSeries.series.length > 0) {
+
+					this.chartSerieName = res.data.optionsSeries.options
+					this.chartDonutData = res.data.optionsSeries.series
+				} 
+				this.chartDonutOptions = { labels: this.chartSerieName }
+				this.donutSeries = this.chartDonutData
 			})
 		},
 		resetSearch() {
@@ -223,10 +248,9 @@ export default {
             this.$store.commit('toggleCollapse')
         },
 	},
-	computed: {
-		icon() {
-            return this.$store.state.isCollapseVisible ? "fa-angle-up" : "fa-angle-down"
-        }
+	beforeMount() {
+		this.loadHeaders()
+		this.loadData()
 	},
 	mounted: function() {
 		const vm = this;
@@ -236,17 +260,13 @@ export default {
 				vm.fields = response.data
 		})
 	},
-	beforeMount() {
-		this.loadHeaders()
-		this.loadData()
-	}
 }
 </script>
 
 <style>
 
 .table-hour-geral {
-  font-size: 0.8em;
+  font-size: 1em;
 }
 
 .table-title {
@@ -264,6 +284,11 @@ export default {
   text-align: center;
   background-color: lightyellow;
   vertical-align: middle !important;
+}
+
+.total-hours {
+	font-size: 1.2em;
+	font-weight: 700;
 }
 
 </style>
